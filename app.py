2d9dbb02f0a3c6bf5507a0a2dd82766ae957d7e2
LoickDev97438 <loickcherimont@gmail.com>
2022-01-03 15:18:25 +0400

    Send project to github

---
+++ app.py
@@ -0,0 +1,24 @@
+from tkinter import *
+from functions import divider_list
+
+# create a root
+root = Tk()
+
+# add title to window
+root.title("Dividers of ...")
+
+# root dimensions
+root.geometry("1080x720")
+root.minsize(720,480)
+
+#  - customization (Saturday-Sunday) - 
+# add a iconbitmap about maths
+
+
+# test function from functions.py file
+# divider_list(user_entry)
+
+
+# display the root
+root.mainloop()
+
