a669d99751ad61d53c883dd2d0db67b8ba22e14c
LoickDev97438 <loickcherimont@gmail.com>
2022-02-10 23:31:54 +0400

    Improve code: Organize it again, add a docstring, change comments

--- app.py
+++ app.py
@@ -1,96 +1,85 @@
-#   ====================
-#   ===    IMPORT    ===
-#   ====================
+# --- IMPORT ---
 from tkinter import *
 from tkinter.font import BOLD
 
-#   ====================
-#   === xxx ===
-#   ====================
-
-
-# create a root
-root = Tk()
-
-# add title to window
-root.title(" Dividers of ...")
-
-# root dimensions
-root.geometry("1080x720")
-root.minsize(720,480)
-
-# customization
-root.config(bg='#00bbff')
-
-# add a iconbitmap about maths
-root.iconbitmap('pi-icon.ico')
-
-# --- FRAME ---
-# Create a app-box with title, button, entry
-app_box = Frame(root, bg="#00204a", padx=25, pady=25)
-
-user_entry = Entry(app_box, justify='center', fg="black", bg="#e7eaf6", font=('Courier', 25, BOLD))
-
-divider_output = Label(root, bg="#00bbff", font=("Courier", 20, BOLD))
-
+# --- DIVIDER LIST function ---
 def divider_list():
+    """Set output an error message or informations about dividers of entry value
 
-    # check if entry is not empty
-    # if(user_entry['text'] == ""):
-    #     divider_output['fg'] = '#f23557'
-    #     divider_output['text'] = "The entry is required!"
-
+    No key argument
+    """
+    # Try get 'wrapper_entry' into integer
     try:
-        int(user_entry.get())
+        number = int(wrapper_entry.get())
+
+    # Raised exception :
+    # reset 'wrapper_entry', show error message
+    # stop the function
     except:
-        # print('Empty entry or invalid value!')
-        user_entry['text'] = ""
-        divider_output['fg'] = '#f23557'
-        divider_output['text'] = "Enter a valid value!"
+        wrapper_entry['text'] = ''
+        output['fg'] = '#f23557'
+        output['text'] = "Please enter a valid value!"
+        return
 
-    finally:
-        number = int(user_entry.get())
-        # core function :
-        match number:
-        # if user_entry == 0 -> return "INFINITE NUMBER"
-            case 0:
-                divider_output['fg'] = 'white'
-                divider_output['text'] = "INFINITE NUMBER OF DIVIDERS"
-            # elif user_entry == 1 -> return "1"
-            case 1:
-                divider_output['fg'] = 'white'
-                divider_output['text'] = number
-            # else user_entry > 1
-            case _:
-                divider_output['fg'] = 'white'
-                dividers = [str(i) for i in range(1,number+1) if number % i == 0]
-                dividers = " - ".join(dividers)
-                divider_output['text'] = dividers
+    # No exception
+    output['fg'] = 'white' # In visible color
 
-#   ====================
-#   === xxx ===
-#   ====================
+    # In function number value
+    # execute corresponding statement
+    match number:
+        case 0:
+            output['text'] = 'INFINITE NUMBER OF DIVIDERS'
+        case 1:
+            output['text'] = f'Unique divider:\n {number}'
+        case _:
+            # Convert integer into string
+            # to use join method on 'dividers'
+            dividers = [str(i) for i in range(1,number+1) if number % i == 0]
+            dividers = " - ".join(dividers)
+            output['text'] = f'List of dividers:\n {dividers}'
 
-# Add a Main title to the frame
-main_title = Label(app_box, text="DIVIDER(S)", fg="white", bg="#00204a", font=('Courier', 50, BOLD))
-main_title.pack()
 
-# instruction for the user
-instruction = Label(app_box, text="Please, enter an integer", fg="#fdb44b", bg="#00204a", font=('Courier', 18, BOLD))
-instruction.pack()
+# ************** CORE OF THE APPLICATION **************
 
-# add a entry field in the frame
-user_entry.pack(pady=10)
+# --- MAIN WINDOW ---
+# Create window and set title
+window = Tk()
+window.title(" Dividers of ...")
 
-# add a button to launch the function divider_list
-launcher = Button(app_box, text="Launch!", fg="#00204a", bg="#fdb44b", font=('Courier', 25, BOLD), command=divider_list)
-launcher.pack(pady=15)
+# Set default and minimal dimensions of the window
+window.geometry("1080x720")
+window.minsize(720,480)
 
-# add app_box frame
-app_box.pack(expand=TRUE)
+# Customization of window
+window.config(bg='#00bbff')
+window.iconbitmap('img/pi-icon.ico')
 
-# label to display the divider(s)
-divider_output.pack(expand=TRUE, fill=BOTH)
 
-# display the root
-root.mainloop()
+# --- FRAME AND COMPONENTS ---
+wrapper = Frame(window, bg="#00204a", padx=25, pady=25)
+
+# Give wrapper a title
+wrapper_title = Label(wrapper, text="DIVIDER(S)", fg="white", bg="#00204a", font=('Courier', 50, BOLD))
+wrapper_title.pack()
+
+# Say user what to do
+wrapper_instruction = Label(wrapper, text="Please, enter an integer", fg="#fdb44b", bg="#00204a", font=('Courier', 18, BOLD))
+wrapper_instruction.pack()
+
+# Create a entry for user
+wrapper_entry = Entry(wrapper, justify='center', fg="black", bg="#e7eaf6", font=('Courier', 25, BOLD))
+wrapper_entry.pack(pady=10)
+
+# Add a launcher of 'divider_list' function
+wrapper_btn = Button(wrapper, text="Launch!", fg="#00204a", bg="#fdb44b", font=('Courier', 25, BOLD), command=divider_list)
+wrapper_btn.pack(pady=15)
+
+# Add wrapper and all its components into main window
+wrapper.pack(expand=TRUE)
+
+# Display divider(s)
+output = Label(window, bg="#00bbff", font=("Courier", 20, BOLD))
+output.pack(expand=TRUE, fill=BOTH)
+
+# Display main window
+window.mainloop()
